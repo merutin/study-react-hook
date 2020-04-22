42ec4f62bbaa0903e1b58c9e290cfcc42078bad8
merutin <merutin@gmail.com>
2020-04-22 23:50:50 +0900

    end 2.2

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import WithHooks from './WithHooks';
+import WithoutHooks from './WithoutHooks';
+
+const App: React.FC = () => {
+  return (
+  <div style={{ display: 'grid' }}>
+    <div style={{ gridColumn: '1/2' }}>
+      <div>クラスコンポーネント版</div>
+      <WithoutHooks />
+    </div>
+    <div style={{ gridColumn: '2/2' }}>
+      <div>React Hooks版</div>
+      <WithHooks />
+    </div>
+  </div>
+  );
+};
+
+export default App;
---
+++ src/WithHooks.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+const useInput = (defaultValue: string) => {
+  const [value, setValue] = useState(defaultValue);
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setValue(e.target.value);
+  }
+  return { value, onChange };
+};
+
+const App: React.FC = () => {
+  const name = useInput("serval");
+  const type = useInput("friends");
+
+  useEffect(() => {
+    document.title = `${name.value} is ${type.value}`;
+  }, [name, type]);
+
+  return (
+    <div>
+      <input {...name} />
+      <input {...type} />
+    </div>
+  );
+};
+
+export default App;
