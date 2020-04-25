1bc1035c55bab21c2725e51ea90bc1892ad77437
merutin <merutin@gmail.com>
2020-04-25 16:39:01 +0900

    end 3.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
-import React, { useContext } from "react";
+import React from "react";
+import { History } from "history";
 
-import { FirebaseContext } from "./Firebase";
 import { FirebaseAuth, signInWithRedirect, signOut } from "./FirebseAuth";
 
-const Content: React.FC = () => {
-  const {userId, userName} = useContext(FirebaseContext);
-  return (
-    <div>
-      {userName} ({userId}) is signedIn
-    </div>
-  );
+import { Router } from "./Router";
+
+const Content: React.FC<{ history: History }> = ({ history }) => {
+  return <Router history={history} />;
 };
 
-const App: React.FC = () => {
+const App: React.FC<{ history: History }> = ({ history }) => {
   const NotSignIn = React.useCallback(() => {
-    return <button onClick={() => signInWithRedirect()}>signIn</button>
+    return <button onClick={() => signInWithRedirect()}>signIn</button>;
   }, []);
   const Loading = React.useCallback(() => {
-    return <div>loading now ...</div>
+    return <div>loading now ...</div>;
   }, []);
 
   return (
     <FirebaseAuth NotSignedIn={NotSignIn} Loading={Loading}>
-      <Content />
+      <Content history={history} />
       <button onClick={signOut}>sign out</button>
     </FirebaseAuth>
-  )
-}
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const history = createBrowserHistory();
+
+ReactDOM.render(<App history={history} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
