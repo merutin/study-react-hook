98eb7bfe1630faee9d3f79a69a7a9e86a7955f39
merutin <merutin@gmail.com>
2020-05-02 16:21:11 +0900

    5.2

---
+++ sandbox-editor/src/App.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+
+import { runJSTest } from "./sandbox/javascript";
+
+const initialSources: { [p: string]: string } = {
+  "index.test.js": `const { answer } = require('index.js')
+  
+  describe('computed on The Earth', () => {
+      test('Life, the Universe, and Everything is 42.', () => {
+    expect(answer()).toBe(42)
+  })
+  })
+  `,
+  "index.js": `function answer() {
+    return 8 * 6;
+  }
+
+  module.exports = {
+    answer
+  }
+  `,
+};
+
+const App: React.FC = () => {
+  const [stdout, setStdout] = useState("");
+
+  useEffect(() => {
+    runJSTest(initialSources, "index.test.js", setStdout);
+  }, []);
+
+  return (
+    <code>
+      <pre>{stdout}</pre>
+    </code>
+  );
+};
+
+export default App;
