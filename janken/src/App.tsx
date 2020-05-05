6c29d6276fe87875b5f01f0795bc9863028d70cf
merutin <merutin@gmail.com>
2020-05-05 22:01:20 +0900

    react hooks 1st

--- janken/src/App.tsx
+++ janken/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
 interface JankenProps {
@@ -7,47 +7,7 @@ interface JankenProps {
   judgment?: number;
 }
 
-interface JankenState {
-  human?: number;
-  computer?: number;
-  judgment?: number;
-}
-
-class JankenGamePage extends Component<JankenProps, JankenState> {
-  constructor(props: JankenProps) {
-    super(props);
-    this.state = { human: undefined, computer: undefined };
-  }
-
-  pon(human_hand: number) {
-    const computer_hand = Math.floor(Math.random() * 3);
-    this.setState({ human: human_hand, computer: computer_hand });
-  }
-
-  judge(): number | undefined {
-    if (this.state.human === undefined || this.state.computer === undefined) {
-      return undefined;
-    } else {
-      return (this.state.computer - this.state.human + 3) % 3;
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>じゃんけん ポン！</h1>
-        <JankenBox actionPon={(te: any) => this.pon(te)} />
-        <ScoreBox
-          human={this.state.human}
-          computer={this.state.computer}
-          judgment={this.judge()}
-        />
-      </div>
-    );
-  }
-}
-
-const JankenBox = (props: { actionPon: (arg0: number) => void }) => {
+const JankenBox: React.FC<{ actionPon: (te: number) => void }> = (props) => {
   return (
     <div>
       <button onClick={() => props.actionPon(0)}>グー</button>
@@ -57,7 +17,7 @@ const JankenBox = (props: { actionPon: (arg0: number) => void }) => {
   );
 };
 
-const ScoreBox = (props: JankenProps) => {
+const ScoreBox: React.FC<JankenProps> = (props) => {
   const teString = ["グー", "チョキ", "パー"];
   const judgmentString = ["引き分け", "勝ち", "負け"];
 
@@ -85,8 +45,37 @@ const ScoreBox = (props: JankenProps) => {
   );
 };
 
-function App() {
-  return <JankenGamePage />;
-}
+const App: React.FC = () => {
+  const [te, setTe] = React.useState<JankenProps>();
+
+  const pon = (human_hand: number) => {
+    const computer_hand = Math.floor(Math.random() * 3);
+    setTe({ human: human_hand, computer: computer_hand });
+  };
+
+  const judge = (): number | undefined => {
+    if (
+      te === undefined ||
+      te.human === undefined ||
+      te.computer === undefined
+    ) {
+      return undefined;
+    } else {
+      return (te.computer - te.human + 3) % 3;
+    }
+  };
+
+  return (
+    <div>
+      <h1>じゃんけん ポン！</h1>
+      <JankenBox actionPon={(te: any) => pon(te)} />
+      <ScoreBox
+        human={te !== undefined ? te.human : undefined}
+        computer={te !== undefined ? te.computer : undefined}
+        judgment={judge()}
+      />
+    </div>
+  );
+};
 
 export default App;
